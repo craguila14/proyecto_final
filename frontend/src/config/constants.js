0b842b8c31115e1aa99c3079b86644b63e518dcb
craguila14 <craguilaasenjo@gmail.com>
2025-08-14 02:53:23 -0400

    merge commit

--- frontend/src/config/constants.js
+++ frontend/src/config/constants.js
@@ -6,7 +6,7 @@ const getHostIP = () => {
   return match ? match[0] : hostname;
 }
 
-export const URLBASE = `http://${getHostIP()}:3000` // ruta del backend usando solo IP
+export const URLBASE = 'https://proyecto-final-7ttv.onrender.com'
 //export const URLBASE = 'https://shortenqr.com/api' // ruta del backend
 
 export const ENDPOINT = {
